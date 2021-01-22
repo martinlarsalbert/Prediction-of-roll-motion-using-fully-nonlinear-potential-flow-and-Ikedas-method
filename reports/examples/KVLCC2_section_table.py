4fcfc29aed43ce7d8fef599a2f8e6522f5673169
Martin Alexandersson <marale@kth.se>
2021-01-22 10:10:35 +0100

    Wrote some more.

---
+++ reports/examples/KVLCC2_section_table.py
@@ -0,0 +1,69 @@
+import pandas as pd
+from src.helpers import calculate_lewis
+from rolldecayestimators import ikeda_naked
+from reports import mdl_results    
+
+
+def get():
+
+    df_kvlcc2 = pd.read_csv('../../data/interim/kvlcc_areas.csv', sep=';', index_col=0)
+
+    df_kvlcc2.rename(columns={
+        'b':'beam',
+        't':'T',
+        'r_b':'R',
+    }, inplace=True)
+    df_kvlcc2['sigma']=df_kvlcc2.area/(df_kvlcc2.beam*df_kvlcc2['T'])
+    meta_data = mdl_results.df_rolldecays.iloc[0]
+    OG = meta_data.TA-meta_data.kg
+    df_kvlcc2['OG/d']=OG/df_kvlcc2['T']
+    scale_factor = meta_data.scale_factor
+
+    df_kvlcc2['area']/=(scale_factor**2)
+    df_kvlcc2['x']/=scale_factor
+    df_kvlcc2['T']/=scale_factor
+    df_kvlcc2['beam']/=scale_factor
+    df_kvlcc2['R']/=scale_factor
+
+    df_ = df_kvlcc2.copy()
+    df_.rename(columns={
+        'beam':'B',
+        'T':'d',
+    }, inplace=True)
+
+    a, a_1, a_3, sigma_s, H = calculate_lewis(df_)
+    df_kvlcc2['a_1'] = a_1
+    df_kvlcc2['a_3'] = a_3
+    df_kvlcc2['H0'] = H
+
+    return df_kvlcc2
+
+def show():
+
+    df_kvlcc2 = get()
+
+    interesting = [
+    'x',
+    'beam',
+    'T',
+    'sigma',
+    'OG/d',
+    'R',
+    'a_1',
+    'a_3']
+
+    section_table = df_kvlcc2[interesting].copy()
+    rename = {
+        'x':r'$x$',
+        'beam':r'$beam$',
+        'T':r'$T_s$',
+        'sigma':r'$\sigma$',
+        'OG/d':r'$\frac{OG}{d}$',
+        'R':r'$R_b$',
+        'a_1':r'$a_1$',
+        'a_3':r'$a_3$',
+
+    }
+    section_table.rename(columns=rename, inplace=True)
+    return section_table
+
--- reports/examples/decision_tree.py
+++ reports/examples/decision_tree.py
@@ -2,6 +2,10 @@ import joblib
 from sklearn import tree
 import matplotlib.pyplot as plt
 
+import rolldecayestimators.ikeda_naked as ikeda_naked
+
+from reports.examples import KVLCC2_section_table
+
 
 def show():
     c_r_tree = joblib.load('../../models/C_r_tree.pkl')
@@ -9,3 +13,26 @@ def show():
     fig.set_size_inches(15,8)
     #with plt.style.context('paper'):
     tree.plot_tree(c_r_tree, ax=ax, feature_names=[r'$\sigma$', r'$a_3$'], rounded=True);
+
+def show_KVLCC2_C_r_prediction():
+
+
+    df_kvlcc2 = KVLCC2_section_table.get()
+    df_kvlcc2_ = df_kvlcc2.copy()
+    OG = df_kvlcc2_['OG/d']*df_kvlcc2_['T']
+    ra = 1000
+    df_kvlcc2_['C_r'] = ikeda_naked.calculate_C_r(bwl=df_kvlcc2_.beam,
+                          a_1=df_kvlcc2_.a_1, a_3=df_kvlcc2_.a_3, sigma=df_kvlcc2_.sigma, 
+                                              H0=df_kvlcc2_.H0, d=df_kvlcc2_['T'], OG=OG, 
+                          R=df_kvlcc2_.R, ra=ra)
+
+    good_feature_names=['sigma', 'a_3']
+    c_r_tree = joblib.load('../../models/C_r_tree.pkl')
+    df_kvlcc2['C_r'] = c_r_tree.predict(X=df_kvlcc2[good_feature_names])
+
+    fig,ax=plt.subplots()
+    df_kvlcc2.plot(y='C_r', style='.-', label='decision tree', ax=ax)
+    df_kvlcc2_.plot(y='C_r', style='.-', label='ikeda', ax=ax)
+    ax.set_ylabel(r'$C_r$')
+    ax.set_xlabel(r'station')
+    ax.grid(True)
